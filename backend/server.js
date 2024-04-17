0f0807778b75790668d914551fab227cd371d915
mrshabbarkhan <mrshabbarofficial@gmail.com>
2024-04-17 16:13:43 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -11,35 +11,34 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 // DB Connection
 connectDB();
-// static file
-app.use(express.static("public"));
-app.use("/uploads", express.static("uploads"));
+// // static file
+// app.use(express.static("public"));
+// app.use("/uploads", express.static("uploads"));
 
-// Body Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// // Body Parser
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// User Routes
-app.use("/api/user", require("./routes/userRoutes"));
+// // User Routes
+// app.use("/api/user", require("./routes/userRoutes"));
 
-// Configure Multer for file uploads
-app.get("/", (req, res) => {
-  res.json({
-    msg: "WELCOME TO DOCUMENT API",
-  });
-});
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-// Error Handler
-app.use(errorHandler);
+// // Configure Multer for file uploads
+// app.get("/", (req, res) => {
+//   res.json({
+//     msg: "WELCOME TO DOCUMENT API",
+//   });
+// });
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+// // Error Handler
+// app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT : ${PORT}`.bgBlue.white);
 });
-
