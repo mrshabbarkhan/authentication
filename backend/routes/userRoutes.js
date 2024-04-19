60490c950070c8966c9814784f1718e5f9687d2d
mrshabbarkhan <mrshabbarofficial@gmail.com>
2024-04-19 16:45:44 +0530

    last one

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 // User Register
 router.post("/register", registerUser);
-router.post("/register", (req, res) => {
+router.get("/register", (req, res) => {
   res.json({
     msg: "WELCOME TO REGISTER API, PLEASE MAKE A POST REQUEST",
   });
 });
 router.post("/login", loginUser);
-router.post("/login", (req, res) => {
+router.get("/login", (req, res) => {
   res.json({
     msg: "WELCOME TO LOGIN API, PLEASE MAKE A POST REQUEST",
   });
