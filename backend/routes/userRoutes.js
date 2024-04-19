dc8cb1037beaa3ee129b86c193af1a145f14c2c8
mrshabbarkhan <mrshabbarofficial@gmail.com>
2024-04-19 16:41:08 +0530

    last commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,15 @@ const router = express.Router();
 
 // User Register
 router.post("/register", registerUser);
+router.post("/register", (req, res) => {
+  res.json({
+    msg: "WELCOME TO REGISTER API, PLEASE MAKE A POST REQUEST",
+  });
+});
 router.post("/login", loginUser);
+router.post("/login", (req, res) => {
+  res.json({
+    msg: "WELCOME TO LOGIN API, PLEASE MAKE A POST REQUEST",
+  });
+});
 module.exports = router;
